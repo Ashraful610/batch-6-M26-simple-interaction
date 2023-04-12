b1585ae202a32cc1442f9c48ea7dc35b1c423fed
Ashraful610 <ashalam610@gmail.com>
2023-04-12 22:25:24 +0600

    form condition is done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const submitButton = document.getElementById('submit-btn');
+
+submitButton.addEventListener('click', function() {
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    if(emailValue === 'islam@gmail.com' && passwordValue === 'aaaaaa'){
+        console.log('email and password is correct' , emailValue , passwordValue);
+    }
+    else if(emailValue !== 'islam@gmail.com' && passwordValue !== 'aaaaaa'){
+        console.log('email and password is incorrect' , emailValue , passwordValue);
+    }        
+    else if (emailValue !== 'islam@gmail.com'){
+        console.log('email is incorrect ' , emailValue);
+    }
+    else{
+        console.log('password is incorrect ' , passwordValue);
+    }
+})
