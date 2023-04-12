790afddddb518fdce5d9abd27d0553143f2cc137
Ashraful610 <ashalam610@gmail.com>
2023-04-13 01:34:54 +0600

    collect all element in js

---
+++ js/bank.js
@@ -0,0 +1,8 @@
+const depositMoney = document.getElementById('deposit-money');
+const withdrawMoney = document.getElementById('withdraw-money');
+const totalBalance = document.getElementById('total-balance');
+const depositField = document.getElementById('deposit-field');
+const depositButton = document.getElementById('deposit-btn');
+const withdrawField = document.getElementById('withdraw-field');
+const withdrawButton = document.getElementById('withdraw-btn');
+
