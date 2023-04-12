a4a6a7bfc7de80c7163fb80bc01257938fd6dae3
Ashraful610 <ashalam610@gmail.com>
2023-04-13 02:29:54 +0600

    deposit functionalyti is done

--- js/bank.js
+++ js/bank.js
@@ -6,3 +6,9 @@ const depositButton = document.getElementById('deposit-btn');
 const withdrawField = document.getElementById('withdraw-field');
 const withdrawButton = document.getElementById('withdraw-btn');
 
+
+depositButton.addEventListener('click', function(){
+    depositMoney.innerText = parseInt(depositField.value) + parseInt(depositMoney.innerText)
+    totalBalance.innerText = parseInt(totalBalance.innerText) + parseInt(depositField.value)
+    depositField.value = ''
+})
